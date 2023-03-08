daf7fa9d8d31e52f2bb7777756e22e84dbe8d959
makuke1234 <makuke1234@gmail.com>
2023-03-08 13:17:34 +0200

    Deleted unnecessary shit

--- PrjHinnakalkulaator/IDistantsiTeisendaja.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Interface IDistantsiTeisendaja
-    Function KilometersToMiles(ByVal km As Double) As Double
-    Function MilesToKilometers(ByVal mi As Double) As Double
-End Interface
--- PrjHinnakalkulaator/IKiiruseTeisendaja.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Interface IKiiruseTeisendaja
-    Function KmhToMs(ByVal kmh As Double) As Double
-    Function MsToKmh(ByVal ms As Double) As Double
-End Interface
-

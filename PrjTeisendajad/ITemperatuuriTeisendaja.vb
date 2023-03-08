52a0f6462754cf8a068711473b5a689e73566c0b
makuke1234 <makuke1234@gmail.com>
2023-03-08 13:43:33 +0200

    Empty

--- PrjTeisendajad/ITemperatuuriTeisendaja.vb
+++
@@ -1,4 +0,0 @@
-﻿Public Interface ITemperatuuriTeisendaja
-    Function CelciusToFahrenheit(ByVal c As Double) As Double
-    Function FahrenheitToCelsius(ByVal f As Double) As Double
-End Interface

ccd99067b83b725d216f9fa840d2b1d50c58faeb
makuke1234 <makuke1234@gmail.com>
2023-03-15 17:53:12 +0200

    Test

---
+++ ClassTest/IClassTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    interface IClassTest
+    {
+        int arvuta(int arv1, int arv2);
+    }
+}
--- Kasutajaliides/Form1.cs
+++ Kasutajaliides/Form1.cs
@@ -10,11 +10,20 @@ using System.Windows.Forms;
 
 namespace Kasutajaliides
 {
+
     public partial class Form1 : Form
     {
+        protected ClassTest.CClassTest component = new ClassTest.CClassTest();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int arv = component.arvuta(1, 2);
+            MessageBox.Show("Hello world " + arv.ToString(), "title", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }

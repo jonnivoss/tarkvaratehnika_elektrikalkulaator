566f3fb056b77dec728928500ec063c626872c4e
makuke1234 <makuke1234@gmail.com>
2023-03-29 17:26:58 +0300

    Sätte küsimine + muutmine

---
+++ AndmeSalvestaja/CAS.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndmeSalvestaja
+{
+    public class CAS : AndmeSalvestaja.IAS
+    {
+        private string path = "";
+        private string[] setMap = new string[(int)ASSetting.size];
+
+        public CAS(string savepath)
+        {
+            this.path = savepath;
+        }
+
+        public bool loadFile()
+        {
+            return true;
+        }
+        public bool saveFile()
+        {
+            return true;
+        }
+
+        public bool changeSetting(ASSetting setting, string value)
+        {
+            int iset = (int)setting;
+            if (iset >= (int)ASSetting.size)
+            {
+                return false;
+            }
+
+            try
+            {
+                this.setMap[iset] = value;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public string getSetting(ASSetting setting)
+        {
+            int iset = (int)setting;
+            if (iset >= (int)ASSetting.size)
+            {
+                return null;
+            }
+
+            return this.setMap[iset];
+        }
+    }
+}
---
+++ AndmeSalvestaja/IAS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndmeSalvestaja
+{
+    public enum ASSetting
+    {
+        test0,
+        test1,
+
+        size
+    };
+
+    interface IAS
+    {
+        bool loadFile();
+        bool saveFile();
+
+        bool changeSetting(ASSetting setting, string value);
+        string getSetting(ASSetting setting);
+    }
+}

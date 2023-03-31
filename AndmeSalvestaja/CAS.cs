0b68f3e71381361d324814c30792adfe27877db7
makuke1234 <makuke1234@gmail.com>
2023-03-31 12:46:41 +0300

    loadFile tehtud

--- AndmeSalvestaja/CAS.cs
+++ AndmeSalvestaja/CAS.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace AndmeSalvestaja
 {
     public class CAS : AndmeSalvestaja.IAS
     {
         private string path = "";
-        private string[] setMap = new string[(int)ASSetting.size];
+        private Dictionary<ASSetting, string> setMap = new Dictionary<ASSetting, string>
+        {
+            { ASSetting.test0, "" },
+            { ASSetting.test1, "" }
+        };
 
         public CAS(string savepath)
         {
@@ -18,7 +24,21 @@ namespace AndmeSalvestaja
 
         public bool loadFile()
         {
-            return true;
+            if (this.path.Length == 0)
+            {
+                return false;
+            }
+
+            try
+            {
+                var contents = File.ReadAllText(this.path);
+                this.setMap = JsonConvert.DeserializeObject<Dictionary<ASSetting, string>>(contents);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
         public bool saveFile()
         {
@@ -27,15 +47,14 @@ namespace AndmeSalvestaja
 
         public bool changeSetting(ASSetting setting, string value)
         {
-            int iset = (int)setting;
-            if (iset >= (int)ASSetting.size)
+            if ((int)setting >= (int)ASSetting.size)
             {
                 return false;
             }
 
             try
             {
-                this.setMap[iset] = value;
+                this.setMap[setting] = value;
                 return true;
             }
             catch (Exception)
@@ -46,13 +65,12 @@ namespace AndmeSalvestaja
 
         public string getSetting(ASSetting setting)
         {
-            int iset = (int)setting;
-            if (iset >= (int)ASSetting.size)
+            if ((int)setting >= (int)ASSetting.size)
             {
                 return null;
             }
 
-            return this.setMap[iset];
+            return this.setMap[setting];
         }
     }
 }

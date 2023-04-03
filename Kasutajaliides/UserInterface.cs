d389eea872d0380688a4134acd2105874c5d7262
AneeMandmets <anee.mandmets@gmail.com>
2023-04-03 17:35:20 +0300

    CŠV faili nupp

--- Kasutajaliides/UserInterface.cs
+++ Kasutajaliides/UserInterface.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace Kasutajaliides
 {
-
+    
     public partial class UserInterface : Form
     {
+        private Andmepyydja.CAP AP = new Andmepyydja.CAP();
         private void btnAvaCSV_Click(object sender, EventArgs e)
         {
+            AP.chooseFile();
 
         }
     }

15ac553a43d7f4e4da28544dd74fe44c8d89eeac
joskar <jomeli@ttu.ee>
2023-04-17 22:10:33 +0300

    asd

--- Arvutaja/IArvutaja.cs
+++ Arvutaja/IArvutaja.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Arvutaja
 {
+    using VecT = System.Collections.Generic.List<System.Tuple<System.DateTime, double>>;
     interface IArvutaja
     {
+        int integreerija(VecT andmed1, VecT andmed2, System.DateTime alumine, System.DateTime ylemine, ref double integraal);
     }
+
 }

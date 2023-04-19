0ad4eafc6dfaf0db53af8e3bb9727380d6d4ac1d
makuke1234 <makuke1234@gmail.com>
2023-04-19 21:54:02 +0300

    Refactor tarbijaandmete CSV parser

--- UnitTestAP/UnitTestAPCSVParser.cs
+++ UnitTestAP/UnitTestAPCSVParser.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-using ValT = System.Tuple<System.DateTime, double>;
+using DatePriceT = System.Tuple<System.DateTime, double>;
 
 using VecT = System.Collections.Generic.List<System.Tuple<System.DateTime, double>>;
 
-namespace Testid
+namespace UnitTestAP
 {
     [TestClass]
-    public class TestAndmePyydja
+    public class UnitTestAPCSVParser
     {
         [DataTestMethod]
         [Timeout(1000)]
-        public void Test_AP_parseContents()
+        public void Test_AP_parseUserData()
         {
             // make content array
             string[] contarr =
@@ -23,7 +23,7 @@ namespace Testid
                     "01.03.2023 00:00; 01.03.2023 01:00; 0,152; 60,07; \n" +
                     "01.03.2023 01:00; 01.03.2023 02:00; 0,104; 60,40; \n" +
                     "01.03.2023 02:00; 01.03.2023 03:00; 0,084; 44,45; ",
-                "asdasdma;asd,,asd,gds,fjgkjadhkjbhxkjcnknbvnd,dv,ad,vsdfjgjhdskfjghkj\n" +   
+                "asdasdma;asd,,asd,gds,fjgkjadhkjbhxkjcnknbvnd,dv,ad,vsdfjgjhdskfjghkj\n" +
                     "Algus;Lõpp;Kogus (kWh);Börsihind (EUR / MWh); \n" +
                     "01.03.2023 00:00; 01.03.2023 01:00; 0,152; 60,07; \n" +
                     "01.03.2023 01:00; 01.03.2023 02:00; 0,104; 60,40; \n" +
@@ -52,7 +52,7 @@ namespace Testid
 
             for (int i = 0; i < contarr.Length; ++i)
             {
-                var vec2 = ap.parseContents(contarr[i]);
+                var vec2 = ap.parseUserData(contarr[i]);
                 CollectionAssert.AreEqual(vecs[i], vec2);
             }
         }
